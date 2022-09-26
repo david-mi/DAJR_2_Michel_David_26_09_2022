a9691496c7bea62f063002c983da7e763820f525
David Michel <david.michel2809@gmail.com>
2022-09-26 10:42:44 +0200

    feat: adding more selectors and improving comments

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,7 +1,14 @@
+// Selecting DOM elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const formDataInputs = document.querySelectorAll(".formData input");
+const form = document.querySelector("form[name=reserve]");
 const navToggleButton = document.querySelector(".icon");
 
 // toggle nav event
 navToggleButton.addEventListener("click", handleTopNavStyle);
+
 /** Handle topNav style */
 function handleTopNavStyle() {
   const topNav = document.getElementById("myTopnav");
@@ -12,17 +19,15 @@ function handleTopNavStyle() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-
-// launch modal event
+// Adding click listener on each modal button
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+/** Displays form modal on click */
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+
+
 
 
