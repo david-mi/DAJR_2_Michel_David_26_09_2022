324b6cafffd881dadcb5ba0ab72f1f84b564a528
David Michel <david.michel2809@gmail.com>
2022-09-27 11:56:58 +0200

    feat: renaming modal.js by index.js and importing needed modules in it. adding listeners

---
+++ starterOnly/index.js
@@ -0,0 +1,41 @@
+import * as validate from "./validator.js";
+import { resetForm, handleFormSubmit } from "./formActions.js";
+// instanciated of DomSelectors class
+import dom from "./domSelectors.js";
+
+/** Handle topNav style */
+function handleTopNavStyle() {
+  const topNav = dom.getOne("myTopnav");
+  if (topNav.className === "topnav") {
+    topNav.className += " responsive";
+  } else {
+    topNav.className = "topnav";
+  }
+}
+
+dom.navToggleButton.addEventListener("click", handleTopNavStyle);
+
+/** Displays form modal on click */
+function launchModal() {
+  dom.modalbg.classList.remove("select-hide");
+}
+
+dom.modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Adding click listener to closeModalButton
+dom.closeModalButton.addEventListener("click", handlecloseModalButton);
+
+/** hide form modal on click */
+function handlecloseModalButton() {
+  dom.modalbg.classList.add("select-hide");
+  resetForm();
+}
+
+dom.form.addEventListener("submit", handleFormSubmit);
+dom.formInputs.forEach(input => input.addEventListener("input", validate[input.name]));
+
+
+
+
+
+
