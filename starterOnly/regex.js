2599baa3ae16648b468a34fceb91731964dc2310
David Michel <david.michel2809@gmail.com>
2022-09-27 11:45:47 +0200

    adding regex file #2

---
+++ starterOnly/regex.js
@@ -0,0 +1,4 @@
+export const nameRegex = /^[a-z-À-ö]{2,}$/i;
+export const emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+export const birthDateRegex = /^\d{4}-\d{2}-\d{2}$/;
+export const tournamentCountRegex = /\b([0-9]|[1-9][0-9])\b/;
