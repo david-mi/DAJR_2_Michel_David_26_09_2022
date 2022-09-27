44b70871272d669234fcee67071b3902f7697a7e
David Michel <david.michel2809@gmail.com>
2022-09-27 11:53:11 +0200

    feat: adding formActions module #2 adding triggerInputEvents,  hydrateFormModel, checkFormModelInvalid, handleFormBody, resetForm and  handleFormSubmit functions to formActions module

---
+++ starterOnly/formActions.js
@@ -0,0 +1,56 @@
+import dom from "./domSelectors.js";
+import { formModel, defaultFormModel } from "./formModel.js";
+
+function triggerInputEvents() {
+  dom.formInputs.forEach(input => input.dispatchEvent(new Event("input")));
+}
+
+export const hydrateFormModel = (propertie, value, isValid) => {
+  formModel[propertie] = {
+    ...formModel[propertie],
+    value,
+    isValid
+  };
+};
+
+export function checkFormModelInvalid() {
+  return (
+    Object
+      .values(formModel)
+      .some(({ isValid }) => isValid === false)
+  );
+}
+
+export function handleFormBody() {
+  const formBody = {};
+  for (const key in formModel) {
+    formBody[key] = formModel[key].value;
+  }
+  console.log("Merci ! Votre réservation a été reçue.");
+  console.log(formBody);
+}
+
+export function resetForm() {
+  dom.formData.forEach(container => {
+    container.removeAttribute("data-error");
+    container.removeAttribute("data-error-visible");
+  });
+
+  for (const key in formModel) {
+    formModel[key].value = defaultFormModel[key].value;
+    formModel[key].isValid = defaultFormModel[key].isValid;
+  }
+
+  dom.form.reset();
+}
+
+/** handling form submission */
+export function handleFormSubmit(event) {
+  event.preventDefault();
+  triggerInputEvents();
+
+  const isFormModelInvalid = checkFormModelInvalid();
+  if (isFormModelInvalid === false) {
+    handleFormBody();
+  }
+}
