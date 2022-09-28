7cdbf2313f49df52638f093083179d88f6d49068
David Michel <david.michel2809@gmail.com>
2022-09-28 16:27:09 +0200

    fix: check if confirmationDiv exist before trying to remove it in closeForm

--- js/form/formDisplay.js
+++ js/form/formDisplay.js
@@ -9,7 +9,9 @@ export function showForm() {
 /** hide form modal on click */
 export function closeForm() {
   dom.modalbg.classList.add("select-hide");
-  const confirmationDiv = dom.getOne(".confirmation");
-  confirmationDiv.remove();
+  const confirmationDiv = dom.getNode(".confirmation");
+  if (confirmationDiv) {
+    confirmationDiv.remove();
+  }
   resetForm();
 }
