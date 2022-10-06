4f3c63008f96e6a580d7f5d4a3e5e2e81f91031f
David Michel <david.michel2809@gmail.com>
2022-10-06 10:11:05 +0200

    refacto: simplifying email regex

--- js/form/validators/regex.js
+++ js/form/validators/regex.js
@@ -1,4 +1,4 @@
 export const nameRegex = /^[a-z-À-ö\s]{2,}$/i;
-export const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+export const emailRegex = /^[a-zA-Z]+[a-z-A-Z.-_\d]+?@[a-zA-Z]+\.[a-z]{2,4}$/;
 export const birthDateRegex = /^\d{4}-\d{2}-\d{2}$/;
 export const tournamentCountRegex = /^\b([0-9]|[1-9][0-9])\b$/;
